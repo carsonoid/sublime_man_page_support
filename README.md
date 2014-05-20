17c564a5efb2fa2d4b5308f98c5410b4d5dbca89
Carson Anderson <carson@betterservers.com>
2014-05-20 16:17:59 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Sublime Man Page Support
+
+Sublime Text 2 Support for Unix style manual pages.
+
+## Inludes:
+
+ * groff syntax highlighting
+ * command to create a new man page skeleton
+ * snippet that will add useful macros to your man pages on older systems (must come before man page content)
---
+++ new_page.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin
+import os, time
+
+class ManPageNewCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        v = self.window.new_file()
+        v.settings().set('default_dir',
+            os.path.join(sublime.packages_path(), 'User'))
+        v.set_syntax_file('Packages/ManPage/man-groff.tmLanguage')
+
+        template = """.\" Manpage for ${1:<COMMAND>}.
+.\" Contact ${2:<AUTHOR_EMAIL>} to correct errors or typos.
+.TH man 8 "%s" "1.0" "${1:<COMMAND>}"
+.SH NAME
+${1:<COMMAND>}
+.SH SYNOPSIS
+.SY
+${1:<COMMAND>}
+.YS
+.SH DESCRIPTION
+${1:<COMMAND>}
+.SH BUGS
+No known bugs.
+.SH SEE ALSO
+.SH AUTHOR
+.MT ${2:<AUTHOR_EMAIL>}
+${3:<AUTHOR_NAME>}
+.ME
+""" %(time.strftime("%B %Y"))
+        v.run_command("insert_snippet", {"contents": template})
