36d725dbc662de66a8a333bdb6cc525b8e40115e
Carson Anderson <carson@betterservers.com>
2014-06-10 14:14:02 -0600

    add man page preview command and syntax Preview can be run via menu, cmd pallete, or as build system

--- new_page.py
+++ new_page.py
@@ -1,5 +1,32 @@
 import sublime, sublime_plugin
 import os, time
+import subprocess
+
+class ManPagePreview(sublime_plugin.WindowCommand):
+    def run(self):
+	# exit if file is dirty, we can't run a man command against a file that doesn't exist
+        if self.window.active_view().is_dirty():
+            o = self.window.get_output_panel("manfail")
+            o.run_command("insert_snippet", {"contents": "Unable to preview unsaved file."})
+            self.window.run_command("show_panel", {"panel": "output.manfail"})
+            return
+
+        # process document with groff
+        curpath = self.window.active_view().file_name()
+        c = subprocess.Popen(["groff", "-Tascii", "-man", curpath], stdout=subprocess.PIPE)
+        output, err = c.communicate()
+
+        # run groff output through col to clean it up
+        col = subprocess.Popen(["col", "-bx"], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        cleanout, err = col.communicate(output)
+
+        # write clean output to new window
+        v = self.window.new_file()
+        v.set_syntax_file('Packages/sublime_man_page_support/man-preview.tmLanguage')
+        e = v.begin_edit()
+        p = v.text_point(0,0)
+        v.insert(e, p, cleanout)
+        v.end_edit(e)
 
 class ManPageNewCommand(sublime_plugin.WindowCommand):
     def run(self):
@@ -8,8 +35,8 @@ class ManPageNewCommand(sublime_plugin.WindowCommand):
             os.path.join(sublime.packages_path(), 'User'))
         v.set_syntax_file('Packages/sublime_man_page_support/man-groff.tmLanguage')
 
-        template = """.\" Manpage for ${1:<COMMAND>}.
-.\" Contact ${2:<AUTHOR_EMAIL>} to correct errors or typos.
+        template = """.\\\" Manpage for ${1:<COMMAND>}.
+.\\\" Contact ${2:<AUTHOR_EMAIL>} to correct errors or typos.
 .TH man 8 "%s" "1.0" "${1:<COMMAND>}"
 .SH NAME
 ${1:<COMMAND>}
